2910d09213b7611543e615b0b1a207a1d7b82196
TeddyKwak <joke79@hanyang.ac.kr>
2024-12-05 14:36:56 +0900

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('EliceLab with NestJS')
+    .setDescription('API developed throughout the API with NestJS course')
+    .setTitle('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(configService.get<number>('BACKEND_PORT'));
 }
 bootstrap();
